ec9f4bb21152d13e261cf06ad0548cbcb536ad21
nimishak02 <nimishacr02@gmail.com>
2025-05-01 20:11:07 +0530

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import { useState } from "react";
 import { assets } from "../assets/assets";
+import Sidebar from "../components/Sidebar";
+import PromptBox from "../components/PromptBox";
 
 export default function Home() {
   const [expand, setExpand] = useState(false);
@@ -11,6 +13,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex h-screen">
+        <Sidebar expand={expand} setExpand={setExpand} />
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image
@@ -24,7 +27,7 @@ export default function Home() {
           {messages.length === 0 ? (
             <>
               <div className="flex items-center gap-3">
-                <Image src={assets.logo_icon} alt="" className="h-16" />
+                <Image src={assets.logo_icon} alt="" className="h-16 w-auto" />
                 <p className="text-2xl font-medium">Hi, I'm DeepSeek</p>
               </div>
               <p className="text-sm mt-2">How can I help you today?</p>
@@ -32,6 +35,10 @@ export default function Home() {
           ) : (
             <div></div>
           )}
+          <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
+          <p className="text-xs absolute bottom-1 text-gray-500">
+            AI-generated, for reference only
+          </p>
         </div>
       </div>
     </div>
